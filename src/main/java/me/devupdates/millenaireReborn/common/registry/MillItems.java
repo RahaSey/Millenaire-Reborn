c6f8edc3b7308e0ac7eeec7282c759920e8e6a2b
Leviaria <113382526+Leviaria@users.noreply.github.com>
2025-07-30 21:01:16 +0200

    Update MillItems.java

--- src/main/java/me/devupdates/millenaireReborn/common/registry/MillItems.java
+++ src/main/java/me/devupdates/millenaireReborn/common/registry/MillItems.java
@@ -1,18 +1,13 @@
 package me.devupdates.millenaireReborn.common.registry;
 
 import me.devupdates.millenaireReborn.MillenaireReborn;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 
 /**
  * Registry für alle Millénaire Items
